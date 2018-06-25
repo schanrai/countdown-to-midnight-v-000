e953ae5c643d810619bb9fa9239961d4069d28d2
Sushi Chanrai <sushi@pathworks.co>
2018-06-25 20:04:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
-def countdown(integer)
-  while integer > 0
-   puts 
+def countdown (number)
+  while number > 0
+   puts "#{number} SECOND(S)!"
    integer -= 1
   end
 end
